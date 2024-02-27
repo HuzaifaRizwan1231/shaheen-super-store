70a8bc833c901f32729eb3467edf1036497e37df
Huzaifa Rizwan <huzaifa.rizwan1231@gmail.com>
2024-02-27 23:50:41 +0500

    set db to local

--- includes/connect.php
+++ includes/connect.php
@@ -1,7 +1,7 @@
 <?php 
 
 
-$con = new mysqli('localhost','id21367706_huzaifa','Huzaifa@1231','id21367706_ecommerce_website');
+$con = new mysqli('localhost','root','','shaheen-super-store');
 
 if(!$con){
    die(mysqli_error($con));
