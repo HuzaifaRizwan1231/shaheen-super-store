e14dc4815c97fb7193de490a2d9e317f7a843a7f
Huzaifa Rizwan <huzaifa.rizwan1231@gmail.com>
2024-02-28 00:01:26 +0500

    fixed naming issue

--- api/admin/index.php
+++ api/admin/index.php
@@ -30,7 +30,7 @@ include '../includes/connect.php';
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="./sidebar.css">
+    <link rel="stylesheet" href="./admin.css">
 
 
 </head>
